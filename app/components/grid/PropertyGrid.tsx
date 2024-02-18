c449b5e901e1f6266b587994093d9c680d8e607f
VarshaKakumanu <varshakakumanu@gmail.com>
2024-02-18 07:37:02 +0530

    changes

--- app/components/grid/PropertyGrid.tsx
+++ app/components/grid/PropertyGrid.tsx
@@ -1,11 +1,11 @@
 // Import your properties data
-import properties from '/properties.json'; // Correct the path to your actual file
+import properties from "/properties.json"; // Correct the path to your actual file
 import PropertyCard from "./PropertyCard";
 
 const PropertyGrid = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {properties.map(property => (
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 content-center'>
+      {properties.map((property) => (
         <PropertyCard key={property.id} property={property} />
       ))}
     </div>
